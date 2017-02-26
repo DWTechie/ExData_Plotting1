bf5a7ade953c61b2f3575930e319f926e8237497
DWTechie <daniel.wollmann@outlook.com>
2017-02-25 16:44:09 -0800

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,6 @@
+setwd("C:/Users/danielsw/OneDrive/Data Science Specialization/Exploratory Data Analisys/Week 1/ExData_Plotting1")
+source("ReadData.R")
+Data <- ReadData("household_power_consumption.txt")
+png(filename = "plot1.png", width = 480, height = 480)
+hist(Data$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
